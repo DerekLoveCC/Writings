f782a04d78acb37b07d63df70c8d8c4427607acc
Derek <zhaomingbo1010@126.com>
2019-06-19 22:06:29 +0800

    Add WPF MVVM Close window article

---
+++ Blog/Code/AStockViewer/AStockViewer.UI/ViewModels/BindableBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace AStockViewer.UI.ViewModels
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaiseProperyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyName)
+        {
+            var bodyExpression = propertyName.Body as MemberExpression;
+            if (bodyExpression?.Member?.Name != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(bodyExpression.Member.Name));
+            }
+        }
+    }
+}
---
+++ Blog/Code/AStockViewer/AStockViewer.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AStockViewer.UI.Helpers;
+using AStockViewer.UI.Views;
+using System;
+using System.Windows.Input;
+
+namespace AStockViewer.UI.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            CloseWindowSampleCommand = new DelegateCommand(OnCloseWindowSample);
+        }
+
+        private void OnCloseWindowSample(object obj)
+        {
+            var window = new DialogCloserSample();
+            window.ShowDialog();
+        }
+
+        public ICommand CloseWindowSampleCommand { get; }
+    }
+}
