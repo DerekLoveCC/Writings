dbba656e6ebc397034b8eb779aa07c485b0859c8
DerekLoveCC <zhaomingbo1010@126.com>
2020-08-01 14:57:47 +0800

    new article

---
+++ Code/DebugWithoutSourceCodeInDotNet/Program.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace DebugWithoutSourceCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var textReader = new StreamReader(@".\test.csv"))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false
+                };
+                var csvReader = new CsvReader(textReader, config);
+                var csvDataReader = new CsvDataReader(csvReader);
+                while (csvDataReader.Read())
+                {
+                    for (int i = 0; i < csvDataReader.FieldCount; i++)
+                    {
+                        Console.Write(csvDataReader.GetString(i) + " ");
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
